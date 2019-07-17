3cc1f3867ec8e9578696de02d461aa1095b67ddb
coffee347 <coffee351@outlook.com>
2019-07-18 00:41:19 +0300

    ver 1.3

---
+++ js/slick-init.js
@@ -0,0 +1,16 @@
+﻿    $(document).ready(function(){
+      $('.Slider-Section').slick({
+        autoplay: false,
+        autoplaySpeed: 4500,
+        draggable: false,
+        centerMode: true,
+        variableWidth: true,
+        arrows: true,
+        dots: true,
+        vertical: false,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        infinite: false});
+});
